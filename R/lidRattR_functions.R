550ef9c9a654431b3206ea4802fb1917e190cd49
RCBlackburn <32521096+RCBlackburn@users.noreply.github.com>
2024-05-20 16:00:01 -0700

    new functions

--- R/lidRattR_functions.R
+++ R/lidRattR_functions.R
@@ -1,19 +1,19 @@
 #' Individual voxel-based SVi function
 #'
 #' @description This function takes the height values (Z) for each lidar point point cloud and
-#' is used within lidR::voxel_metrics() to calculate the sub-voxel i (SVi) density (SVi_d) and a list of Z values for for each sub-voxel.
+#' is used within lidR::voxel_metrics() to calculate the sub-voxel i (SVi) density (SViD) and a list of Z values for for each sub-voxel.
 #' @param z height
-#' @keywords lidar voxel metrics
+#' @keywords lidar voxel
 #' @export
 #' @examples
 #' voxels <- voxel_metrics(las, func = vox_mt(Z), res = 2)
 
 vox_mt <- function(z)
 {
-  SVi_D = length(z)
+  SViD = length(z)
 
   metrics =list(
-    SVi_D = SVi_D,
+    SViD = SViD,
     z_list = list(z)
   )
   return(metrics)}
@@ -21,13 +21,13 @@ vox_mt <- function(z)
 #' Individual voxel-based variable function
 #'
 #' @description This function provides individual voxel-based variables including the frequency ratio (FRDi),frequency ratio (FRSVi),
-#' number of returns below each sub-voxel (SVi) (PDi_below), and the number of returns above each SVi (PDi_above),
-#' sum of returns within a sub-voxels (SV_sum), classifcation of a sub-voxel maximum (SVM),
-#' median height of the number of returns above (SVM_md_above) or within (SVM_md) SVMs
+#' number of returns below each sub-voxel (SVi) (PDiBelow), and the number of returns above each SVi (PDiAbove),
+#' sum of returns within a sub-voxels (SVsum), classifcation of a sub-voxel maximum (SVM),
+#' median height of the number of returns above (SVMmdAbove) or within (SVMmd) SVMs
 #' @param vox lasmetrics3d object created by lidR::voxel_metrics() using the vox_mt() function
 #' @param res SVi cubic resolution in units of las object
 #' @param max_z if numeric, the final height for all sub-voxels
-#' @keywords lidar voxel metrics
+#' @keywords lidar voxel
 #' @import data.table
 #' @export
 
@@ -44,7 +44,7 @@ vox_mt2 <- function(vox, res, max_z)
   empty_z <- data.frame(X = do.call(rbind, lapply(strsplit(new_id, "-"), `[`, 1)),
                         Y = do.call(rbind, lapply(strsplit(new_id, "-"), `[`, 2)),
                         Z = do.call(rbind, lapply(strsplit(new_id, "-"), `[`, 3)),
-                        SVi_D = 0,
+                        SViD = 0,
                         z_list = NA,
                         id_xy = paste0(do.call(rbind, lapply(strsplit(new_id, "-"), `[`, 1)),
                                        "-",
@@ -54,7 +54,7 @@ vox_mt2 <- function(vox, res, max_z)
   fullvox <- rbind(fullvox, empty_z)
 }
 
-  fullvox$SVi_D[is.na(fullvox$SVi_D)] <- 0
+  fullvox$SViD[is.na(fullvox$SViD)] <- 0
   fullvox[unlist(lapply(fullvox$z_list, is.null)),]$z_list <- NA
   ##### Individual voxel metrics
   ### P_Di is the number of returns below each voxel (Pearse et al. 2019; Kim et al. 2016 uses returns above)
@@ -65,7 +65,7 @@ vox_mt2 <- function(vox, res, max_z)
   # calculate points below each voxel
   fullvox$Z <- as.numeric(fullvox$Z)
   point_blow <- fullvox[,as.list(lapply(unique(Z), function(x){
-    sum(SVi_D[Z < x])})),
+    sum(SViD[Z < x])})),
     by = list(X,Y)]
   colnames(point_blow)[3:ncol(point_blow)] <- paste0(unique(fullvox$Z))
   point_blow <- melt(point_blow, id.vars = c("X","Y"), variable.name = "Z", value.name = "npoints_below")
@@ -79,20 +79,20 @@ vox_mt2 <- function(vox, res, max_z)
   point_above <- fullvox[,as.list(lapply(unique(Z), function(x){
     return(list(
       m_z = median(unlist(z_list[Z >x]), na.rm = TRUE),
-      p_a = sum(SVi_D[Z > x])))}
+      p_a = sum(SViD[Z > x])))}
     )
     ),
     by = list(X,Y)]
 
   colnames(point_above)[3:ncol(point_above)] <- paste0(unique(fullvox$Z))
   points_am <- point_above[seq(1, nrow(point_above), 2), ]
-  points_am <- suppressWarnings(as.data.table(lapply(points_am,  function(x) as.numeric(unlist(x)))))
+  points_am <- suppressWarnings(as.data.table(lapply(points_am,  function(x) unlist(x))))
   points_am[is.na(points_am)] <- 0
   points_am <- melt(points_am, id.vars = c("X","Y"), variable.name = "Z", value.name = "pa_med")
   points_am$id_xyz = paste0(points_am$X,"-",points_am$Y,"-",points_am$Z)
 
   points_ab <- point_above[seq(2, nrow(point_above), 2), ]
-  points_ab <- suppressWarnings(as.data.table(lapply(points_ab,  function(x) as.numeric(unlist(x)))))
+  points_ab <- suppressWarnings(as.data.table(lapply(points_ab,  function(x) unlist(x))))
   points_ab <- melt(points_ab, id.vars = c("X","Y"), variable.name = "Z", value.name = "npoints_above")
   points_ab$id_xyz = paste0(points_ab$X,"-",points_ab$Y,"-",points_ab$Z)
 
@@ -100,17 +100,17 @@ vox_mt2 <- function(vox, res, max_z)
   fullvox <- merge(fullvox, points_ab[,c(4,5,6)], by = "id_xyz")
 
   ### FR_SVi is the frequency ratio of the number of returns in a SVi in relation to total returns in a SV(in Pearse et al. 2019 this is FR_Di)
-  sv_sum <- fullvox[, .(sv_sum = sum(SVi_D)), by = "id_xy"]
-  fullvox <- merge(fullvox, sv_sum, "id_xy")
-  fullvox$FR_SVi = fullvox$SVi_D/fullvox$sv_sum
+  SVsum <- fullvox[, .(SVsum = sum(SViD)), by = "id_xy"]
+  fullvox <- merge(fullvox, SVsum, "id_xy")
+  fullvox$FR_SVi = fullvox$SViD/fullvox$SVsum
   fullvox[is.na(fullvox$FR_SVi)]$FR_SVi <- 0
 
   ### FR_Di is the frequency ratio of the number of returns above a SVi in relation to total returns in a SV (Kim et al. 2016)
-  fullvox$FR_Di = fullvox$npoints_above/fullvox$sv_sum
+  fullvox$FR_Di = fullvox$npoints_above/fullvox$SVsum
   fullvox[is.na(fullvox$FR_Di)]$FR_Di <- 0
 
   ### sub-voxel maximums
-  maxSVM <- fullvox[, .SD[which.max(SVi_D)], by = "id_xy"]
+  maxSVM <- fullvox[, .SD[which.max(SViD)], by = "id_xy"]
   maxSVM[unlist(lapply(maxSVM$z_list, is.null)),]$z_list <- 0
 
   ## SVM_M median height of total returns in SVM (Kim et al. 2016)
@@ -129,15 +129,15 @@ vox_mt2 <- function(vox, res, max_z)
     Y = as.numeric(fullvox$Y),
     Z = as.numeric(fullvox$Z),
     vox_res = res,
-    SVi_D = fullvox$SVi_D,
+    SViD = fullvox$SViD,
     FRDi = fullvox$FR_Di,
     FRSVi = fullvox$FR_SVi,
-    PDi_below = fullvox$npoints_below,
-    PDi_above = fullvox$npoints_above,
-    SV_sum = fullvox$sv_sum,
-    SVM_md_above = fullvox$pa_med,
+    PDiBelow = fullvox$npoints_below,
+    PDiAbove = fullvox$npoints_above,
+    SVsum = fullvox$SVsum,
+    SVMmdAbove = fullvox$pa_med,
     SVM = fullvox$SVM,
-    SVM_md = fullvox$SVM_m
+    SVMmd = fullvox$SVM_m
 
 
   )
@@ -149,12 +149,12 @@ vox_mt2 <- function(vox, res, max_z)
 #' @description This function provides summarized voxel-based variables including the mean, median, variance, standard deviation, coefficient of variation,
 #' IQR, skewness, and kurtosis. Summaries for this function are done to the entire area provided either directly or within the
 #' lidR::pixel_metrics() function.
-#' @keywords lidar voxel metrics
+#' @keywords lidar voxel
 #' @import data.table
 #' @export
 
-vox_sum <- function(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_above, SVM_md, SVM, vox_res){
-  las_vox <- data.table(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_above, SVM_md, SVM)
+vox_sum <- function(SViD,FRDi, FRSVi, PDiBelow, PDiAbove, SVsum, SVMmdAbove, SVMmd, SVM, vox_res){
+  las_vox <- data.table(SViD,FRDi, FRSVi, PDiBelow, PDiAbove, SVsum, SVMmdAbove, SVMmd, SVM)
   SVM <- las_vox[las_vox$SVM == 1]
   SVM$SVM <- NULL
   means <- apply(SVM[,6:ncol(SVM)], 2, mean, na.rm = TRUE)
@@ -178,7 +178,7 @@ vox_sum <- function(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_abov
   names(kurt)<- paste0(names(kurt), "_", vox_res, "_", "kurt")
   data_svm <- c(means, meds, var, sd, cv, IQR, skew, kurt)
 
-  las_vox <- las_vox[,-c("SV_sum", "SVM_md", "SVM", "SVM_md_above")]
+  las_vox <- las_vox[,-c("SVsum", "SVMmd", "SVM", "SVMmdAbove")]
   means <- apply(las_vox, 2, mean, na.rm = TRUE)
   names(means)<- paste0(names(means), "_", vox_res, "_","mean")
   meds <- apply(las_vox, 2, median, na.rm = TRUE)
@@ -211,25 +211,27 @@ vox_sum <- function(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_abov
 #' Voxel point cloud summary function
 #'
 #' @description This function provides individual voxel-based variables including the frequency ratio (FRDi),frequency ratio (FRSVi),
-#' number of returns below each sub-voxel (SVi) (PDi_below), and the number of returns above each SVi (PDi_above),
-#' sum of returns within a sub-voxels (SV_sum), classifcation of a sub-voxel maximum (SVM), and
-#' median height of the number of returns above (SVM_md_above) or within (SVM_md) SVMs. These variables are summarized
-#' for the entir point cloud including the mean, median, variance, standard deviation, coefficient of variation,
+#' number of returns below each sub-voxel (SVi) (PDiBelow), and the number of returns above each SVi (PDiAbove),
+#' sum of returns within a sub-voxels (SVsum), classifcation of a sub-voxel maximum (SVM), and
+#' median height of the number of returns above (SVMmdAbove) or within (SVMmd) SVMs. These variables are summarized
+#' for the entire point cloud including the mean, median, variance, standard deviation, coefficient of variation,
 #' IQR, skewness, and kurtosis
 #' @param las las object
 #' @param vox_res SVi cubic resolution in units of las object
 #' @param max_z If numeric, the final height for all sub-voxels. If NULL, the max height for the entire point cloud
 #' (or chunk for catalog processing) will be used.
-#' @keywords lidar voxel metrics
+#' @keywords lidar voxels
+#' @return A data frame with each column reporting 65 different voxel variables with the naming convention:
+#' (voxel variable)_(voxel resolution)_(summary statistic)
 #' @import data.table
 #' @import lidR
 #' @export
 #' @examples
 #' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
 #' las <- readLAS(LASfile)
-#' las = filter_poi(las, Classification != LASNOISE)
-#' las = normalize_height(las, knnidw(k = 8, p = 2))
-#' las = filter_poi(las, Z < 50 & Z >= 1.37 )
+#' las <- filter_poi(las, Classification != LASNOISE)
+#' las <- normalize_height(las, knnidw(k = 8, p = 2))
+#' las <- filter_poi(las, Z < 50 & Z >= 1.37 )
 #' las <- decimate_points(las, random_per_voxel(res = 1, n = 8))
 #' metrics <- voxel_summary(las, vox_res = 2, max_z = 40)
 #' print(metrics)
@@ -237,17 +239,17 @@ vox_sum <- function(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_abov
 voxel_summary <- function(las, vox_res = vox_res, max_z ){
   vox <- lidR::voxel_metrics(las, func = vox_mt(Z), res = vox_res, all_voxels = TRUE)
   vox_2 <- vox_mt2(vox, res = vox_res, max_z = max_z)
-  out <- LAS(vox_2, header = las@header, crs = las@crs, index = las@index )
-  vox_sum(out$SVi_D, out$FRDi, out$FRSVi, out$PDi_below, out$PDi_above, out$SV_sum, out$SVM_md_above,
-          out$SVM_md, out$SVM, vox_res[1])
+  out <- suppressWarnings(LAS(vox_2, header = las@header, crs = las@crs, index = las@index ))
+  vox_sum(out$SViD, out$FRDi, out$FRSVi, out$PDiBelow, out$PDiAbove, out$SVsum, out$SVMmdAbove,
+          out$SVMmd, out$SVM, vox_res[1])
 }
 
 #' Voxel raster summary function
 #'
 #' @description This function provides individual voxel-based variables including the frequency ratio (FRDi),frequency ratio (FRSVi),
-#' number of returns below each sub-voxel (SVi) (PDi_below), and the number of returns above each SVi (PDi_above),
-#' sum of returns within a sub-voxels (SV_sum), classifcation of a sub-voxel maximum (SVM), and
-#' median height of the number of returns above (SVM_md_above) or within (SVM_md) SVMs. These variables are summarized
+#' number of returns below each sub-voxel (SVi) (PDiBelow), and the number of returns above each SVi (PDiAbove),
+#' sum of returns within a sub-voxels (SVsum), classifcation of a sub-voxel maximum (SVM), and
+#' median height of the number of returns above (SVMmdAbove) or within (SVMmd) SVMs. These variables are summarized
 #' within each pixel including the mean, median, variance, standard deviation, coefficient of variation,
 #' IQR, skewness, and kurtosis
 #' @param las las object
@@ -256,15 +258,17 @@ voxel_summary <- function(las, vox_res = vox_res, max_z ){
 #' @param max_z If numeric, the final height for all sub-voxels. If NULL, the max height for the entire point cloud
 #' (or chunk for catalog processing) will be used.
 #' @keywords lidar voxel metrics
+#' @return returns a SpatRaster with 65 different layers representing voxel variables summarized to the resolution defined in (rast_res) with the naming convention:
+#' (voxel variable)_(voxel resolution)_(summary statistic)
 #' @import data.table
 #' @import lidR
 #' @export
 #' @examples
 #' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
 #' las <- readLAS(LASfile)
-#' las = filter_poi(las, Classification != LASNOISE)
-#' las = normalize_height(las, knnidw(k = 8, p = 2))
-#' las = filter_poi(las, Z < 50 & Z >= 1.37 )
+#' las <- filter_poi(las, Classification != LASNOISE)
+#' las <- normalize_height(las, knnidw(k = 8, p = 2))
+#' las <- filter_poi(las, Z < 50 & Z >= 1.37 )
 #' las <- decimate_points(las, random_per_voxel(res = 1, n = 8))
 #' metrics <- voxel_raster(las, vox_res = 2, rast_res = 10, max_z = 40)
 #' plot(metrics)
@@ -275,50 +279,22 @@ voxel_raster <- function(las, vox_res = vox_res, rast_res =  rast_res, max_z ){
   vox_2 <- vox_mt2(vox, res = vox_res, max_z = max_z)
   out <- LAS(vox_2, header = las@header, crs = las@crs, index = las@index)
   raster <- pixel_metrics(out,
-                          func = vox_sum(SVi_D,FRDi, FRSVi, PDi_below, PDi_above, SV_sum, SVM_md_above, SVM_md, SVM, vox_res = vox_res[1]),
+                          func = vox_sum(SViD,FRDi, FRSVi, PDiBelow, PDiAbove, SVsum, SVMmdAbove, SVMmd, SVM, vox_res = vox_res[1]),
                           res = rast_res)
 }
 
-
-#' Tree variables to point cloud
-#'
-#' @description This function creates a point cloud with tree merics that can be summarized
-#' @param las las object
-#' @keywords lidar tree metrics
-#' @import data.table
-#' @import tidyr
-#' @import lidR
-#' @export
-#' @examples
-#' las <- readLAS(LASfile)
-#' las_tree <- segment_trees(las, li2012())
-#' las_tree <- ind_tree(las_tree)
-#'
-ind_tree <- function(las){
-  trees <- lidR::crown_metrics(las, .stdtreemetrics)
-  locs <- data.frame(sf::st_coordinates(trees), trees[c(3,4)])
-  locs <- locs[-6]
-  names(locs)[5] <- "ca"
-  trees_las <- suppressWarnings(lidR::LAS(locs, header = las@header, crs = las@crs, index = las@index))
-}
-
-
-
 #' Tree-based variable summary statistics
 #'
-#' @description This function provides summarized tree-based variables  for each of the variables created using the
-#' ind_tree() function. Summary statistics include the mean, standard deviation, and coefficient of variation.
-#' @param las las object from ind_tree() function
-#' @keywords lidar tree metrics
+#' @description This function provides summarized tree-based variables including the mean, standard deviation, and coefficient of variation.
+#' Summaries for this function are done to the entire area provided either directly or within the
+#' lidR::pixel_metrics() function.
+#' @param columns from the lidR::crown_metrics() function (i.e., X, Y, Z, npoints, ca)
+#' @keywords lidar tree
 #' @export
-#' @examples
-#' las <- readLAS(LASfile)
-#' las_tree <- segment_trees(las, li2012())
-#' las_tree <- ind_tree(las_tree)
-#' plot_t_metrics <- tree_sum(las_tree)
 
-tree_sum <- function(las_tree){
-  data <- las_tree@data
+
+tree_sum <- function(X,Y,Z,npoints,ca){
+  data <- data.table(X, Y, Z, npoints, ca)
   tree_metrics <- data.frame(
     ntrees = length(data$Z),
     ht_mean =  mean(data$Z, na.rm = TRUE),
@@ -333,33 +309,68 @@ tree_sum <- function(las_tree){
   return(tree_metrics)
 }
 
-#' Tree-based variable summary statistics for lidR::pixel_metrics() function
+#' Tree point cloud summary function
 #'
-#' @description This function provides summarized tree-based variables just as the tree_sum() function.
-#' This can be used in conjunction is lidR::pixel_metrics() to make raster layers of tree-based summary statistics.
-#' @param Z height
-#' @param npoints number of points
-#' @param ca crown area
-#' @keywords lidar tree metrics
+#' @description This function provides individual tree-based variables including number of trees (ntrees),
+#' height of trees (ht), number of points per tree (npts), and crown area (ca). These variables are summarized
+#' for the entire point cloud including the mean, median, variance, standard deviation, coefficient of variation,
+#' IQR, skewness, and kurtosis
+#' @param las las object
+#' @keywords lidar tree
+#' @import data.table
+#' @import tidyr
+#' @import lidR
 #' @export
 #' @examples
+#' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
 #' las <- readLAS(LASfile)
+#' las <- filter_poi(las, Classification != LASNOISE)
+#' las <- normalize_height(las, knnidw(k = 8, p = 2))
+#' las <- filter_poi(las, Z < 50 & Z >= 1.37 )
+#' las <- decimate_points(las, random_per_voxel(res = 1, n = 8))
 #' las_tree <- segment_trees(las, li2012())
-#' las_tree <- ind_tree(las_tree)
-#' t_metrics <- pixel_metrics(las_tree, tree_sum_raster(Z, npoints, ca), res = 20)
-#'
+#' tree_metrics <- tree_summary(las_tree)
+#' print(tree_metrics)
 
-tree_sum_raster <- function(Z, npoints, ca){
-  tree_metrics <- list(
-    ntrees = length(Z),
-    ht_mean =  mean(Z, na.rm = TRUE),
-    ht_sd = sd(Z, na.rm = TRUE),
-    ht_cv = sd(Z, na.rm = TRUE)/mean(Z, na.rm = TRUE),
-    npts_mean = mean(npoints, na.rm = TRUE),
-    npts_sd = sd(npoints, na.rm = TRUE),
-    npts_cv = sd(npoints, na.rm = TRUE)/mean(npoints, na.rm = TRUE),
-    ca_mean = mean(ca),
-    ca_sd = sd(ca),
-    ca_cv = sd(ca, na.rm = TRUE)/mean(ca, na.rm = TRUE))
-  return(tree_metrics)
+tree_summary <- function(las){
+  trees <- lidR::crown_metrics(las, .stdtreemetrics)
+  locs <- data.frame(sf::st_coordinates(trees), trees[c(3,4)])
+  locs <- locs[-6]
+  names(locs)[5] <- "ca"
+  trees_las <- suppressWarnings(lidR::LAS(locs, header = las@header, crs = las@crs, index = las@index))
+  tree_sum(trees_las$X,trees_las$Y,trees_las$Z,trees_las$npoints, trees_las$ca)
+}
+
+#' Tree raster summary function
+#'
+#' @description This function provides individual tree-based variables including number of trees (ntrees),
+#' height of trees (ht), number of points per tree (npts), and crown area (ca). These variables are summarized
+#' within each pixel including the mean, median, variance, standard deviation, coefficient of variation,
+#' IQR, skewness, and kurtosis
+#' @param las las object
+#' @keywords lidar tree metrics
+#' @import data.table
+#' @import tidyr
+#' @import lidR
+#' @export
+#' @examples
+#' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
+#' las <- readLAS(LASfile)
+#' las <- filter_poi(las, Classification != LASNOISE)
+#' las <- normalize_height(las, knnidw(k = 8, p = 2))
+#' las <- filter_poi(las, Z < 50 & Z >= 1.37 )
+#' las <- decimate_points(las, random_per_voxel(res = 1, n = 8))
+#' las_tree <- segment_trees(las, li2012())
+#' tree_metrics <- tree_raster(las_tree)
+#' plot(tree_metrics)
+
+tree_raster <- function(las, rast_res){
+  trees <- lidR::crown_metrics(las, .stdtreemetrics)
+  locs <- data.frame(sf::st_coordinates(trees), trees[c(3,4)])
+  locs <- locs[-6]
+  names(locs)[5] <- "ca"
+  trees_las <- suppressWarnings(lidR::LAS(locs, header = las@header, crs = las@crs, index = las@index))
+  raster <- pixel_metrics(trees_las,
+                          func = tree_sum(X,Y,Z,npoints, ca),
+                          res = rast_res)
 }
